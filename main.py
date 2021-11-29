e2d962b8efd717d499efc4b910b06a16914d7eb4
Tristan <tschartman41@gmail.com>
2021-11-29 10:00:44 -0500

    idsk

--- main.py
+++ main.py
@@ -1,85 +1,5 @@
 ASS, MAT, LOG, INT, STR, BOL, FLO, LST, DCT, PAR, DOT, SEP, SYM, SLF, OBJ, NUL, KWD, EQU, FUN, REF, CON = "ASS", "MAT", "LOG", "INT", "STR", "BOL", "FLO", "LST", "DCT", "PAR", "DOT", "SEP", "SYM", "SLF", "OBJ", "NUL", "KWD", "EQU", "FUN", "REF", "CON"
 
-class Object ():
-    def __init__ (self, type : str, value):
-        self.type = type
-        self.value = value
-
-class Class (Object):
-    def __init__ (self, methods : dict = None, properties : dict = None):
-        super().__init__("class", None)
-        self.methods = {} if methods == None else methods
-        self.properties = {} if properties == None else properties
-    def call (self, method : str) -> list:
-        return self.methods[method]
-    def fetch (self, property : str):
-        return self.properties[property]
-
-class Type (Class):
-    def __init__ (self, type, value):
-        super().__init__()
-        self.type = type
-        self.value = value
-    def __repr__ (self):
-        return f"{self.__class__.__qualname__}({self.value})"
-
-class String (Type):
-    def __init__ (self, value : str):
-        super().__init__("string", value)
-        self.properties["length"] = len(self.value)
-        self.methods.update({
-            "split" : ("self", "sep", "maxsep=-1", "python {return self.value.split(sep, maxsep)}"),
-            "rsplit" : ("self", "sep", "maxsep=-1", "python {return self.value.rsplit(sep, maxsep)}"),
-            "indexOf" : ("self", "item", "python {try:\n\treturn self.value.index(item)\nexcept:\n\treturn -1}"),
-            "slice" : ("self", "start=0", "end=-1", "python {return self.value[start:end]}")
-        })
-    def __repr__ (self):
-        return f"String(\"{self.value}\")"
-
-class Int (Type):
-    def __init__ (self, value : int):
-        super().__init__("int", value)
-        self.methods.update({
-            "toString" : ("self", "python {return str(self.value)}")
-        })
-
-class Float (Type):
-    def __init__ (self, value : float):
-        super().__init__("float", value)
-        self.methods.update({
-            "toString" : ("self", "python {return str(self.value)}")
-        })
-
-class Bool (Type):
-    def __init__ (self, value : bool):
-        super().__init__("bool", value)
-
-class List (Type):
-    def __init__ (self, value : list):
-        super().__init__("list", value)
-        self.methods.update({
-            "join" : ("self", "j=','", "python {v=self.value.copy()\nfor i in range(len(v)):\n\tif type(v[i])!=str:\n\t\tv[i]=str(v[i])\nreturn j.join(v)}"),
-            "slice" : ("self", "start=0", "end=-1", "python {return self.value[start:end]}"),
-            "pop" : ("self", "item=-1", "python {return self.value.pop(item)}"),
-            "push" : ("self", "...items", "python {for item in items:\n\tself.value.push(item)}")
-        })
-
-class Dict (Type):
-    def __init__ (self, value : dict):
-        super().__init__("dict", value)
-        self.methods.update({
-            "keys" : ("self", "python {return list(self.value.keys())}"),
-            "values" : ("self", "python {return list(self.value.values())}"),
-            "entries" : ("self", "python {entries=list(self.value.entries())\nl=[]\nfor entry in entries:\n\tl.append(list(entry))\nreturn l}"),
-            "pop" : ("self", "item", "python {return self.value.pop(item)}")
-        })
-
-class Func (Object):
-    def __init__ (self, value : str):
-        super().__init__("func", value)
-    def call (self):
-        return self.value
-
 class Token ():
     def __init__ (self, type, value):
         self.type = type
@@ -89,16 +9,49 @@ class Token ():
     def __repr__ (self):
         return f"({self.type}, \"{self.value}\")"
 
+class Namespace ():
+    def __init__ (self, value):
+        self.value = value
+        self.audit = False
+    def keys (self):
+        return self.value.keys()
+    def __getitem__ (self, key):
+        if self.audit:
+            print(key, self.value[key], "NAMESPACE GET")
+        return self.value[key]
+    def __setitem__ (self, key, value):
+        self.value[key] = value
+        if self.audit:
+            print(key, value, "NAMESPACE SET")
+
+class NamespaceList ():
+    def __init__ (self, consts):
+        self.scopes = [Namespace(consts), Namespace({})]
+        self.scopes[1].audit = True
+    def __len__ (self):
+        return len(self.scopes)
+    def __getitem__ (self, ind):
+        return self.scopes[ind]
+    def __setitem__ (self, ind, value):
+        self.scopes[ind] = value
+
 class Interpreter ():
     def __init__ (self, filename="code"):
         self.lines = []
-        self.keywords = ("func", "if", "elif", "else", "for", "while", "in", "break", "continue", "python", "search", "switch", "return", "case", "default", "class", "global", "flag")
+        self.keywords = ("func", "if", "elif", "else", "for", "while", "in", "break", "continue", "python", "search", "switch", "return", "case", "default", "class", "global", "flag", "audit")
         self.flags = {"vars":False}
         self.nonmod = (REF, NUL, INT, STR, PAR, DCT, LST, BOL, FLO, OBJ, KWD)
         # sets up variable scopes, top level scope is readonly constants and second scope is the program global scope, all other scopes are local scopes
-        self.scopes = [{"true":Token(BOL, True), "false":Token(BOL, False), "void":Token(NUL, None)}, {}]
+        self._scopes = NamespaceList({"true":Token(BOL, True), "false":Token(BOL, False), "void":Token(NUL, None)})
+        # self._scopes = [{"true":Token(BOL, True), "false":Token(BOL, False), "void":Token(NUL, None)}, {}]
         self._getData(filename)
         self.run()
+    @property
+    def scopes (self):
+        return self._scopes
+    @scopes.setter
+    def vars (self, value):
+        self._scopes = value
     def _getData (self, filename : str):
         f = open(filename+("" if filename.endswith(".spp") else ".spp"))
         data = f.read()
@@ -183,17 +136,20 @@ class Interpreter ():
                         break
                     ci += 1
                 if not found:
-                    raise Exception("unclosed string")
+                    # unclosed string
+                    self.err(1)
                 tokens.append(Token(STR, '"'+line[i+1:ci]+'"'))
                 i = ci
             elif char.isdigit():
                 fin = ""
                 s = i
                 deciuse = False
-                while line[s].isdigit():
+                while line[s].isdigit() and s < len(line):
                     c = line[s]
                     fin += c
                     s += 1
+                    if s >= len(line):
+                        break
                     if line[s] == ".":
                         if deciuse:
                             break
@@ -204,6 +160,8 @@ class Interpreter ():
                         deciuse = True
                         fin += "."
                         s += 1
+                    if s >= len(line):
+                        break
                 i = s
                 tokens.append(Token(FLO if deciuse else INT, float(fin) if deciuse else int(fin)))
             else:
@@ -244,7 +202,7 @@ class Interpreter ():
                     continue
                 ti = i
                 while ti < len(line):
-                    if not line[ti].isalnum():
+                    if not line[ti].isalnum() and not line[ti] == "_":
                         break
                     ti += 1
                 if ti != i:
@@ -274,17 +232,33 @@ class Interpreter ():
         if type(result) == str:
             result = '"' + result + '"'
         return Token(NUL, None)
+    def deref (self, token):
+        for i in range(len(self.scopes)):
+            scope = self.scopes[-1-i]
+            if token.value in scope.keys():
+                return scope[token.value]
+        self.err(2)
     def domod (self, base, op, mod):
+        print(mod, "r")
+        if mod.type == REF:
+            mod = self.deref(mod)
+        print(mod, "dr")
+        if base.type == REF:
+            base = self.deref(base)
         isstr = False
-        if type(base.value) == str:
+        modstr = False
+        if base.type == STR:
             isstr = True
             base.value = base.value[1:-1]
-        if type(mod.value) == str:
+        if mod.type == STR:
+            modstr = True
             mod.value = mod.value[1:-1]
         op = op.value
         result = None
         if op == "+":
+            print(base.value, "BASE", mod.value, "MOD")
             result = base.value + mod.value
+            print(base.value, mod.value, result, "RESULTANT")
         elif op == "-":
             result = base.value - mod.value
         elif op == "*":
@@ -295,10 +269,14 @@ class Interpreter ():
             result = base.value % mod.value
         if isstr:
             result = '"' + result + '"'
+        if modstr:
+            mod.value = '"' + mod.value + '"'
         return Token(base.type, result)
     def getexp (self, tokens, start):
         i = start+2
         result = tokens[start+1]
+        if result.type == REF:
+            result = self.deref(result)
         while i < len(tokens):
             token = tokens[i]
             if token.type in self.nonmod:
@@ -308,7 +286,53 @@ class Interpreter ():
                 break
             result = ret
             i += 2
-        return result
+        return result, i
+    def getfunc (self, tokens, start):
+        i = start+2
+        depth = 1
+        args = []
+        build = []
+        ftoks = []
+        result = None
+        while i < len(tokens):
+            token = tokens[i]
+            if token.type == PAR:
+                if token.value == "(":
+                    depth += 1
+                else:
+                    depth -= 1
+                if depth == 0:
+                    break
+            elif token.type == SEP:
+                args.append(tuple(build))
+                build = []
+            else:
+                build.append(token)
+            i += 1
+        depth = 1
+        i += 1
+        while i < len(tokens):
+            token = tokens[i]
+            if token.type == DCT:
+                if token.value == "{":
+                    depth += 1
+                else:
+                    depth -= 1
+                if depth == 0:
+                    break
+            else:
+                ftoks.append(token)
+            i += 1
+        result = Token(FUN, (tuple(args), tuple(ftoks)))
+        print(result)
+        return result, i
+    def err (self, code):
+        if code == 0:
+            raise NameError("cannot assign to constant value")
+        elif code == 1:
+            raise SyntaxError("unclosed string")
+        elif code == 2:
+            raise NameError("variable name not defined")
     def evaltokens (self, tokens):
         if type(tokens) == str:
             tokens = self.tokenize(tokens)
@@ -324,19 +348,31 @@ class Interpreter ():
                         tokens[tind] = self.pythonFunc(tokens[tind:])
                     tokens.pop(tind+1)
                 elif token.value == "func":
-                    pass
+                    # print(token)
+                    name = tokens[tind+1].value
+                    if name in self.scopes[0].keys():
+                        # assignment to constant
+                        self.err(0)
+                    self.scopes[-1][name], tind = self.getfunc(tokens, tind)
+                elif token.value == "audit":
+                    self._printvars()
             elif token.type == CON:
                 self.flags[token.value[0]] = {"on":True, "off":False, "switch":not self.flags[token.value[0]]}[token.value[1]]
             elif token.type == ASS:
                 if token.value == "=":
                     if self.tokens[tind-1].value in self.scopes[0].keys():
-                        raise NameError("cannot assign to constant value")
-                    self.scopes[-1][self.tokens[tind-1].value] = self.getexp(tokens, tind)
+                        # assignment to constant
+                        self.err(0)
+                    self.scopes[-1][self.tokens[tind-1].value], tind = self.getexp(tokens, tind)
                 else:
-                    tokens.insert(Token(MAT, token.value[0]), i+1)
-                    tokens.insert(Token(REF, tokens[tind-1].value), i+2)
-                    tokens[i] = Token(ASS, "=")
+                    tokens.insert(tind+1, Token(MAT, token.value[0]))
+                    tokens.insert(tind+1, Token(REF, tokens[tind-1].value))
+                    tokens[tind] = Token(ASS, "=")
+                    print(tokens[tind:])
                     continue
+            elif token.type == FUN:
+                pass
+                # self.runfun
             tind += 1
     def _printvars (self):
         scopes = self.scopes[1:]
@@ -346,7 +382,6 @@ class Interpreter ():
             for key in scope.keys():
                 print(f"\t{key} : {scope[key]}")
     def run (self):
-        exline = 0
         self.evaltokens(self.lines)
         if self.flags["vars"]:
             self._printvars()
